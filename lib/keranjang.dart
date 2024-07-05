c70f2968cb1603b7a0ea337836b335f91bcd61f9
doraotari <doraotari@gmail.com>
2024-07-05 08:30:31 +0000

    pertemuan 5 juli

--- lib/keranjang.dart
+++ lib/keranjang.dart
@@ -7,27 +7,103 @@ class KeranjangPage extends StatelessWidget {
   const KeranjangPage({super.key});
 
   @override
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Keranjang'),
       ),
-      body: Consumer<ProviderProduk>(
-        builder: (context, produk, child) {
-          return ListView.builder(
-            itemCount: produk.jumlahProduk,
-            itemBuilder: (context, index) {
-              return ListTile(
-                leading: CircleAvatar(
-                  foregroundImage: NetworkImage(produk.listProduk[index].image),
+      body: Column(
+        children: [
+          Expanded(
+            child: Consumer<ProviderProduk>(
+              builder: (context, produk, child) {
+                return ListView.builder(
+                  itemCount: produk.jumlahProduk,
+                  itemBuilder: (context, index) {
+                    return Dismissible(
+                      onDismissed: (DismissDirection direction) {
+                        if (direction == DismissDirection.endToStart) {
+                          Provider.of<ProviderProduk>(context, listen: false)
+                              .hapusKeranjang(produk.listProduk[index]);
+                        }
+                      },
+                      direction: DismissDirection.endToStart,
+                      background: Container(
+                        alignment: Alignment.centerRight,
+                        color: Colors.red,
+                        child: Icon(
+                          Icons.delete,
+                          size: 30,
+                          color: Colors.white,
+                        ),
+                      ),
+                      key: ValueKey(produk.listProduk[index].id),
+                      child: ListTile(
+                        leading: Container(
+                          width: 40,
+                          height: 40,
+                          child: Image.network(produk.listProduk[index].image),
+                        ),
+                        title: Text(produk.listProduk[index].title),
+                        subtitle:
+                            Text(formatRupiah(produk.listProduk[index].price)),
+                      ),
+                    );
+                  },
+                );
+              },
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            height: 100,
+            child: Column(
+              children: [
+                const Divider(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text('Total Qty'),
+                    Consumer<ProviderProduk>(
+                        builder: (context, value, child) =>
+                            Text(value.jumlahProduk.toString())),
+                  ],
                 ),
-                title: Text(produk.listProduk[index].title),
-                subtitle: Text(formatRupiah(produk.listProduk[index].price)),
-                trailing: Text("1"),
-              );
-            },
-          );
-        },
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text('Total Bayar'),
+                    Consumer<ProviderProduk>(
+                        builder: (context, value, child) =>
+                            Text(value.totalHarga)),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 100,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton.icon(
+                    style: const ButtonStyle(
+                      backgroundColor:
+                          WidgetStatePropertyAll(Colors.purpleAccent),
+                      foregroundColor: WidgetStatePropertyAll(Colors.white),
+                    ),
+                    onPressed: () {},
+                    icon: const Icon(Icons.pin_drop),
+                    label: const Text('Cash on Delivery')),
+                ElevatedButton.icon(
+                    onPressed: () {},
+                    icon: const Icon(Icons.qr_code),
+                    label: const Text('Qr Code')),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
--- lib/provider_produk.dart
+++ lib/provider_produk.dart
@@ -8,8 +8,23 @@ class ProviderProduk extends ChangeNotifier {
 
   int get jumlahProduk => _listProduk.length;
 
-  void masukKeranjang(Produk produk) {
+  String get totalHarga => totalBayar();
+
+  totalBayar(){
+    num totalBayar = 0;
+    for (var produk in _listProduk) {
+      totalBayar += produk.price;
+    }
+    return formatRupiah(totalBayar);
+  }
+
+  void masukKeranjang(Produk produk){
     listProduk.add(produk);
+    notifyListeners();
+  }
+
+  void hapusKeranjang(Produk produk) {
+    listProduk.remove(produk);
     notifyListeners();
   }
 }
